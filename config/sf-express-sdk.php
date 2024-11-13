f49396531281bc26ade99ef9df5a8be8919261c3
michael-tscholl <michael-tscholl@users.noreply.github.com>
2024-11-13 06:41:49 +0000

    Fix styling

--- config/sf-express-sdk.php
+++ config/sf-express-sdk.php
@@ -3,7 +3,7 @@
 // config for SmartDato/SfExpress
 return [
     'app' => [
-        'key'=> env('SF_EXPRESS_API_KEY'),
-        'secret'=> env('SF_EXPRESS_SECRET'),
-    ]
+        'key' => env('SF_EXPRESS_API_KEY'),
+        'secret' => env('SF_EXPRESS_SECRET'),
+    ],
 ];
